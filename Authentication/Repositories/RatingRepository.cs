9c67eab91125e30ec721b5e57d007d793f0d9dec
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2026-03-19 09:30:32 +0200

    Implement repository and unit of work patterns
    Added repository interfaces and implementations for generic, idea, rating, and team entities. Introduced UnitOfWork to manage repositories and database transactions. Registered UnitOfWork in DI container. Refactored for improved code organization and testability.

---
+++ Authentication/Repositories/RatingRepository.cs
@@ -0,0 +1,28 @@
+﻿using BlindIdea.API.Core;
+using BlindIdea.API.Entities;
+using BlindIdea.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlindIdea.API.Repositories
+{
+    public class RatingRepository : GenericRepository<Rating>, IRatingRepository
+    {
+        public RatingRepository(AppDbContext context):base(context) { }
+        public async Task<Rating?> GetUserRatingAsync(string userId, string ideaId)
+            => await _context.Ratings
+            .FirstOrDefaultAsync(r => r.UserId == userId && r.IdeaId == ideaId);
+        public async Task<double> GetAverageRatingAsync(string ideaId)
+        {
+            var rating = await _context.Ratings
+                .Where(r => r.IdeaId == ideaId)
+                .ToListAsync();
+            return rating.Any() ? rating.Average(r => r.Score) : 0;
+        }
+        public async Task<IEnumerable<Rating>> GetIdeaRatingsAsync(string ideaId)
+            => await _context.Ratings
+            .Where(r => r.IdeaId == ideaId)
+            .ToListAsync();
+
+        
+    }
+}
